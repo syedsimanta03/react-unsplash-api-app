5aa2bbbc24fd614cc9db795391127d949d04a476
Simanta <syed.simanta06@gmail.com>
2019-07-16 17:15:44 +0600

    'api'

---
+++ build/precache-manifest.14a22b2282690efaf5153da04b3dbfbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ca9ecaaf36168b90fd82fd096f8d28",
+    "url": "/react-unsplash-api-app/index.html"
+  },
+  {
+    "revision": "52e8a1a5616e8fa2ea35",
+    "url": "/react-unsplash-api-app/static/css/main.8e6027c9.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/react-unsplash-api-app/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "52e8a1a5616e8fa2ea35",
+    "url": "/react-unsplash-api-app/static/js/main.47135e36.chunk.js"
+  },
+  {
+    "revision": "06305c8cb447080e5947",
+    "url": "/react-unsplash-api-app/static/js/runtime~main.8f3c05b9.js"
+  }
+]);
---
+++ src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+	state = { term: '' };
+
+	onFormSubmit = event => {
+		event.preventDefault();
+		this.props.onSubmit(this.state.term)
+	};
+	render() {
+		return (
+			<div className="ui segment">
+				<form onSubmit={this.onFormSubmit} className="ui form">
+					<label htmlFor="text">Image Search</label>
+					<input
+						type="text"
+						value={this.state.term}
+						onChange={e => this.setState({ term: e.target.value })}
+					/>
+				</form>
+			</div>
+		);
+	}
+}
